c7c3f458126552d440092e33a9c84a9c57ac36b9
Chris at work <litmusik@yahoo.com>
2013-11-08 16:56:07 -0600

    Started signup page

--- signup.php
+++ signup.php
@@ -1,5 +1,28 @@
-<?php
-	include_once("header.php");
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="css/main.css" rel="stylesheet" type="text/css">
+	<title>GameFace: Sign up</title>
+</head>
+<body>
+	<?php include_once("php_includes/header.php"); ?>
+	<div id="headings">
+		<h2>Sign Up For Free Now!</h2>
+		<h2>Connect With Gamers Across KU!</h2>
+	<div id="view">
+		<div id="signup">
+			<form>
+				
+			</form>
+		</div>
+		<div id="info">
+		
+		</div>
+	</div>
 	
-	include_once("footer.php");
-?>
+	
+	
+	
+	<?php include_once("php_includes/footer.php"); ?>
+</body>
+</html>
