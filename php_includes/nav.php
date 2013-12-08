fd46faa578de7681a3a19eb49addae887ea2f6d1
k174w996 <k174w996@ku.edu>
2013-12-08 11:24:35 -0600

    profile = profile.php

--- php_includes/nav.php
+++ php_includes/nav.php
@@ -1,7 +1,6 @@
-
 <div id="nav">
 	<a href="php_includes/signout.php">Sign Out</a>
 	<a href="./index.php">Home</a>
 	<a href="./editprofile.php">Edit Account</a>
-	<a href="./profile">My Profile</a>
+	<a href="./profile.php">My Profile</a>
 </div>
