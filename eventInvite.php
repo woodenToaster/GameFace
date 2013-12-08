88df03bc9a2efc05c486f578f2e64e8487d0d552
rwerner <r179w024@ku.edu>
2013-12-08 12:33:07 -0600

    Invite friends to events

---
+++ eventInvite.php
@@ -0,0 +1,17 @@
+<?php 
+	$con = mysqli_connect("mysql.eecs.ku.edu", "chogan", "GameFace", "chogan");
+	if (mysqli_connect_errno())
+		{
+		echo "Failed to connect to MySQL:". mysqli_connect_error();
+		}
+
+	$toUser = $_POST['toUser'];
+	$fromUser = $_POST['fromUser'];
+	$text = $_POST['text'];
+	$type = "Invite";
+
+	$x = mysqli_query($con, "INSERT INTO Notifications (fromUser, toUser, type, text) VALUES ('$fromUser','$toUser', '$type','$text');");
+
+	mysqli_close($con);
+	
+?>
