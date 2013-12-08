d392aacfb486b8a22da3237a59aa8b7572c9d4e9
woodenToaster <litmusik@yahoo.com>
2013-12-08 13:13:46 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,29 @@
 <html>
     <head>
-        <link href="css/main.css" rel="stylesheet" type="text/css">
-        <title>GameFace</title>
-    </head>
-    <body>
-		<div id="wrapper">
-			<?php include_once('php_includes/header.php');
-			      include_once('php_includes/nav.php');
-			      include('php_includes/sessions.php'); ?>
-			<div id="view">
-				<?php include_once('php_includes/sidebar.php'); ?>
-				<div id="content">
-					<p>Feed</p>
-				</div>
-			</div>
-		</div>
-		<?php include_once('php_includes/footer.php'); ?>
-    </body>
+                <?php
+                        include_once('php_includes/header.php');
+                        include_once('php_includes/nav.php');
+                        include('php_includes/sessions.php');
+                        require_once('php_includes/defaultDisplay.php');
+                ?>
+<link href="css/main.css" rel="stylesheet" type="text/css">
+<title>GameFace: Home</title>
+                <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+                <script type="text/javascript" src="js/displayNotifications.js"></script>
+</head>
+<body onload='displayNotifications()'>
+                <div id="wrapper">
+                        
+                        <div id="view">
+                                <?php include_once('php_includes/sidebar.php'); ?>
+                                <div id="content">
+                                
+                                        <div id="notifications"></div>
+                                        
+                                        <div id="suggestions"></div>
+                                </div>
+                        </div>
+                </div>
+                <?php include_once('php_includes/footer.php'); ?>
+</body>
 </html>
