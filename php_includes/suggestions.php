a27c196b0e58509ea7f3e3356f1640c97074171e
Chris Hogan <c096h800@gmail.com>
2013-12-08 15:02:08 -0600

    Suggestions working

--- php_includes/suggestions.php
+++ php_includes/suggestions.php
@@ -10,10 +10,23 @@
 	$result = mysqli_query($c,$x);
 	$row = mysqli_fetch_array($result, MYSQLI_NUM);
 	$length = count($row);
-	for($i = 0; $i < $length; $i++) {
-		echo "<div class='notification'>" . $row[$i] . "</div>"; 
-	}
 	
+	//find users to suggest as friends based on top 5
+	for($i = 0; $i < $length; $i++) {
+		$x2 = "SELECT * FROM Accounts WHERE 
+				(game1='$row[$i]'
+				OR game2='$row[$i]'
+				OR game3='$row[$i]'
+				OR game4='$row[$i]'
+				OR game5='$row[$i]')
+				AND (username<>'$uname')";
+				
+		$result2 = mysqli_query($c, $x2);
+		while($row2 = mysqli_fetch_assoc($result2)) {
+			echo "<div class='suggestion'><a href='profile.php?user="
+					. $row2['username']. "'>".$row2['username']."</a> also likes " . $row[$i] . "</div>"; 
+		}
+	}
 	mysqli_close($c);
 	
 ?>
