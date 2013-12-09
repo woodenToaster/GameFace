d34e6faf0cd9da8c006604a947ca3c12b4b3a33b
Chris Hogan <c096h800@gmail.com>
2013-12-08 20:28:15 -0600

    Made logo a link to home

--- php_includes/header.php
+++ php_includes/header.php
@@ -1,5 +1,5 @@
 <div id="header">
-	<img id="logo" height="60"  src="img/GameFaceLogo.png" id="logo">
+	<a href='./index.php'> <img  id="logo" height="60"  src="img/GameFaceLogo.png" id="logo"></a>
 </div>
 <script type="text/javascript" language="javascript" charset"utf-8">
 document.getElementById('logo').style.MozTransform = 'rotate(-10deg)';
