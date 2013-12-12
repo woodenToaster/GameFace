8d55ceacda24fbb51009d1df46a99f775ffb32f5
woodenToaster <litmusik@yahoo.com>
2013-12-12 12:10:40 -0600

    Add comments

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
-<!-- Originally created by Chris Hogan
-	This is the home view of the user profile -->
+<!-- Originally created by Chris Hogan.  This is the home view of the user profile -->
 <html>
     <head>
                 <?php
