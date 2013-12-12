c4163bc9af47ede75df2744fb3ee6f290e64836d
woodenToaster <litmusik@yahoo.com>
2013-12-12 12:20:10 -0600

    Add comments

--- php_includes/header.php
+++ php_includes/header.php
@@ -1,4 +1,6 @@
+<!--Created by Chris Hogan-->
 <!-- edited by Katherine -->
+<!--The header that is displayed on every page-->
 
 <div id="header">
 	<a href='./index.php'> <img  id="logo" height="60"  src="img/GameFaceLogo.png" id="logo"></a>
